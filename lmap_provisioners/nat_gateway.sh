538808de3f2c6796fbb80f87389e98ff44e4d3d2
eugenius <eudriku@gmail.com>
2018-01-12 15:17:57 +0300

    Add vault config and policy files

--- lmap_provisioners/nat_gateway.sh
+++
@@ -1,9 +0,0 @@
-main() {
-  # Enable IP Forwarding
-  sudo sysctl -w net.ipv4.ip_forward=1
-
-  # Masquerade all egress packages as coming from NAT Gateway
-  sudo iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
-}
-
-main
