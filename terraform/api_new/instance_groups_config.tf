3148ce4e3ca3fd974559af06ea1c2260ad16cf2b
Itunuloluwa Fatoki <itunuloluwa.fatoki@gmail.com>
2018-01-19 14:56:03 +0100

    Remove manager from the instance group name

--- terraform/api_new/instance_groups_config.tf
+++ terraform/api_new/instance_groups_config.tf
@@ -1,7 +1,7 @@
 // Setup Instance groups and its dependencies
 
 resource "google_compute_region_instance_group_manager" "lmap_api_instance_group_manager" {
-  name               = "lmap-api-instance-group-manager"
+  name               = "lmap-api-instance-group"
   base_instance_name = "lmap-api-instance-group"
   region = "${var.region}"
   instance_template  = "${google_compute_instance_template.lmap_api_instance_template.self_link}"
