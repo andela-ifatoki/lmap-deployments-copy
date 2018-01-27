9cc08b66a57bcca68f763d498e9385630cd2ec5f
eugenius <eudriku@gmail.com>
2018-01-27 17:17:44 +0300

    Update deploy_staging.sh

--- lmap_ci_config/deploy_staging.sh
+++ lmap_ci_config/deploy_staging.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 add_gcloud_apt_repository(){
+apt-get update
+apt-get install sudo curl -y
 # Create an environment variable for the correct distribution
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+export CLOUD_SDK_REPO="cloud-sdk-xenial"
 
 # Add the Cloud SDK distribution URI as a package source
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -13,17 +15,16 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 install_gcloud(){
 # Update the package list and install the Cloud SDK
-sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+sudo apt-get update && sudo apt-get install google-cloud-sdk=180.0.1-0 -y
 }
 
 configure_gcloud(){
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
-gcloud --quiet config set project noob-183014
-gcloud --quiet config set compute/zone us-east1-b
+gcloud --quiet config set project ${PROJECT_ID}
 }
 
 deploy_change(){
-gcloud compute instances add-metadata api-server --metadata build_commit=${CIRCLE_SHA1}
+gcloud compute project-info add-metadata --metadata build_commit=${CIRCLE_SHA1}
 gcloud beta compute instance-groups managed rolling-action replace lmap-api-instance-group --max-surge=3 --max-unavailable=0 --min-ready=200 --region=europe-west3
 }
 
@@ -31,6 +32,6 @@ main(){
   add_gcloud_apt_repository
   install_gcloud
   configure_gcloud
-  deploy_change  
+  deploy_change
 }
 main
