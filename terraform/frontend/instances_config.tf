ef15bac0b19d7ce078928b7d0ef44ab4c60785ba
Baasbank <baasb4real@yahoo.com>
2018-01-30 16:39:01 +0100

    delete duplicates

--- terraform/frontend/instances_config.tf
+++
@@ -1,6 +0,0 @@
-// Configure the Google Cloud provider
-provider "google" {
-  credentials = "${file("../andela-learning-da1e60624053.json")}"
-  project     = "andela-learning"
-  region      = "europe-west3"
-}
--- terraform/frontend/ip_addresses_config.tf
+++
@@ -1,5 +0,0 @@
-// Configure external IP addresses
-resource "google_compute_address" "ip_ep_frontend_nat" {
-  name = "ip-frontend-nat-gateway"
-  region = "europe-west3"
-}
