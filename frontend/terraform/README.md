e3e40e291cb1e56a8212cc2503b20a6d0ca89fd6
Baasbank <baasb4real@yahoo.com>
2018-01-30 16:37:42 +0100

    add terraform and packer files

---
+++ frontend/terraform/README.md
@@ -0,0 +1,9 @@
+## How To Build The CALM Frontend Infrastructure With Terraform
+
+- Clone this repository by running `git clone https://github.com/andela/lmap-deployments.git` on your terminal.
+- Navigate into the frontend terraform directory by running `cd frontend/terraform` on your terminal
+- Run `terraform init` to initialize terraform and download necessary modules
+- Run `terraform plan` to see a plan of the infrastructure to be built
+- Run `terraform apply`
+
+Voila! Check your Google Cloud console to see your infrastructure once the apply stage is done.
---
+++ frontend/terraform/firewalls_config.tf
@@ -0,0 +1,81 @@
+// Set up project firewalls
+resource "google_compute_firewall" "firewall_frontend_allow_icmp" {
+  name          = "allow-icmp-frontend"
+  description   = "Allow ICMP access across the firewall into the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_http" {
+  name          = "allow-http-frontend"
+  description   = "Allow HTTP access across the firewall into the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["80", "8080"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_tcp_3000" {
+  name          = "allow-tcp-3000-frontend"
+  description   = "Allow TCP access on port 3000 across the firewall into the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["3000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server", "https-server"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_https" {
+  name          = "allow-https-frontend"
+  description   = "Allow HTTPS access across the firewall into the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["443"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https-server"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_private-ssh" {
+  name          = "allow-ssh-private-frontend"
+  description   = "Allow SSH access across the firewall into the Private Subnet of the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["22"]
+  }
+  source_ranges = ["${google_compute_subnetwork.subnet_public_frontend.ip_cidr_range}"]
+  target_tags   = ["private"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_public-ssh" {
+  name          = "allow-ssh-public-frontend"
+  description   = "Allow SSH access across the firewall into the Public Subnet of the Frontend Virtual Private Cloud."
+  network       = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol    = "tcp"
+    ports       = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["public"]
+}
+
+resource "google_compute_firewall" "firewall_frontend_allow_all_out" {
+  name               = "allow-all-out-frontend"
+  description        = "Allow all outbound connections access across the firewall of the Frontend Virtual Private Cloud."
+  direction          = "EGRESS"
+  network            = "${google_compute_network.vpc_frontend.self_link}"
+  allow {
+    protocol         = "all"
+  }
+  destination_ranges = ["0.0.0.0/0"]
+}
