281ad989aecda05d56fdce1c66f3f5fdce723223
Badriku Eugene Noel <eudriku@gmail.com>
2018-02-20 18:03:04 +0300

    Update natgw-provisioner.sh

--- packer/scripts/nat/natgw-provisioner.sh
+++ packer/scripts/nat/natgw-provisioner.sh
@@ -5,6 +5,7 @@ main() {
   sudo mv /tmp/sysctl.conf /etc/sysctl.conf
   # Masquerade all egress packages as coming from NAT Gateway
   sudo iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
+  sudo sysctl -w net.ipv4.ip_forward=1
 }
 
 main
